7502e20fa21405791b7b13e9535c4803b3c9e22a
just-programmer-stack <rukshona.rad.com>
2025-01-04 07:57:33 -0500

    Learnign sytax error

---
+++ python-1.py
@@ -0,0 +1,32 @@
+# ------ SyntaxErrors----------
+
+# 1. 
+if 5 > 2:
+ print("Five is greater than two!")
+# 2 ----- Declearing and varable
+x = 5
+y = "Hello, World!"
+# print variables
+x = 5
+y = "John"
+print(x)
+print(y)
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x,y,z)
+
+# getting date vith type
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+# single or double quotes
+x = "John"
+# is the same as
+x = 'John'
