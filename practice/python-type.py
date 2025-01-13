412d2cdd4c91580f02a3c958c0c5a1c1919976d5
rukshonarad <rukshona.rad@gmail.com>
2025-01-12 22:10:29 -0500

    Learn number types

---
+++ practice/python-type.py
@@ -0,0 +1,37 @@
+# x =5
+# print(type(x))
+# x = 1.10
+# y = 1.0
+# z = -35.59
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
